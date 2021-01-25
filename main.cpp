4abd4047476880a44f7fb02b6dec838a273cbdd6
Awf Wiswasi <55606355+awiswasi@users.noreply.github.com>
2021-01-25 15:03:22 -0800

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,25 @@
+// Test using GITHUB
+// Awf Wiswasi & 
+// Description: Hello World in C++
+
+// ==== MAIN ====
+
+#include <iostream>
+
+using namespace std;
+
+int hello1Function(string name1);
+int hello2Function(string name2);
+
+int main()
+{
+
+    string name1="your name1";
+
+    hello1Function(name1);
+    string name2="your name2";
+    hello2Function(name2);
+
+    return 0;
+
+}
