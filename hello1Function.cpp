7bcac103c067dcde4d9b8a1d567c736cf939bcc1
Awf Wiswasi <55606355+awiswasi@users.noreply.github.com>
2021-01-25 15:07:18 -0800

    Create hello1Function.cpp

---
+++ hello1Function.cpp
@@ -0,0 +1,15 @@
+// Test using GITHUB
+// Author Awf Wiswasi and
+// Description: Hello World in C++
+// ==== hello1Function.cpp ====
+
+#include <iostream>
+
+using namespace std;
+
+int hello1Function(stringname1)
+{
+	cout << "hello World " << name1 << endl;
+	
+	return 0;
+}
